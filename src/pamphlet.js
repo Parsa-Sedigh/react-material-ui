a4d0140776d56ca7396a70e0ade8be5caa22edbd
parsa <parsa.sedigh78@gmail.com>
2021-04-05 00:31:32 +0430

    till start of 13-7. AppBar Transitions:

--- src/pamphlet.js
+++ src/pamphlet.js
@@ -110,4 +110,39 @@ Also delete the logo192 and logo512 then run npm start.
 
 It looks like the create-react-app has been updated to use JS 'strict-mode' by default. Material-UI currently doesn't perfectly support
 strict mode. So for now just remove the <React.StrictMode> tags and leave index the way you see in the video. */
-/* 12- 6. The AppBar Component */
+/* 12- 6. The AppBar Component:
+The first part of our app that we're gonna building is the header. The tutor felt like this is a good place to start since it's the
+one central location that's present on EVERY page and then it's the content under the header which changes(so it's not a good starting
+point!). So it sort of acts as that backbone to the site and just felt like the obvious starting point.
+To build that header though, we're gonna get familiar with the material-ui app-bar comp. This comp provides the basic functionality that you
+would expect out of a header such as spanning the entire length of the screen and always staying at the top.
+
+First create a folder named ui in the src directory and inside that ui folder, create another folder named components. Then move the
+ui folder and App.js inside components folder. So ACTUALLY first create a components folder and then create ui folder in it and also
+move App.js into components folder. Inside ui folder create Header.js .
+
+When you want to return multi line of stuff in a function, you need to wrap those lines inside () .
+After the writing the code in Header file, import and USE that Header comp in App.js without a closing tag so just like:
+...
+<Header /> (so without closing tag).
+hello
+...
+Now you see we have a FULL WIDTH spanning header. You notice that our words hello are actually gone! and it's not ACTUALLY gone but it's
+being covered up by the header, we'll fix that in styling lecture.
+
+Currently, we only have 2 comps in Header.js (AppBar and nested Toolbar), but the first question is, why do we NEED that <Toolbar> comp?
+Because that actually helps lay out everything in a horizontal manner for the <AppBar> whereas if we didn't include that <Toolbar> the
+<AppBar> would start to stack content up, VERTICALLY(so we would have a sidebar instead of a header in that case), when we start to add
+content to it and obviously for an <Appbar> along the top, you want it to be horizontal. So that is why the <Toolbar> is there inside the
+<AppBar>(and that's because to make AppBar horizontal) and with the <AppBar> comp we actually have a default position of fixed(so by default
+by just writing: <AppBar> we ACTUALLY have: <AppBar position="fixed">) which may help to have it included in the code, just so you don't have
+to REMEMBER that that's the default. So when you created an <AppBar> if you were using it's default position, it's good to write that default
+position again, although it doesn't affect anything. If you change that position prop to static, you see that now it's shrunk down(that
+actually doesn't have anything to do with that static value and that's because currently our <body> tag has a default margin of 8px which
+makes the <Appbar> to get some distance from edges of screen) and it doesn't COMPLETELY span that full size and you also see
+our little hello world that we currently have which in the past was covered up but now is underneath the <AppBar> because in source code
+of html it's actually under the <AppBar>. So back it to fixed. Because with fixed value, it would be completely removed out of flow od
+document, the margin of body doesn't affect it and so it seems it's currently spanning all the width of screen.
+
+13-7. AppBar Transitions:
+*/
